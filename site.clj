1ee8377a3409a506921b21b49b28745b9de7e714
Christian Meter <cmeter@googlemail.com>
2022-06-22 17:18:14 +0200

    Print all csv entries as html table

--- site.clj
+++ site.clj
@@ -1,5 +1,6 @@
 (ns site
-  (:require [hiccup2.core :as html]))
+  (:require [hiccup2.core :as html]
+            [timetracker :as tt]))
 
 ;; C-c C-c zum S-Expression evaluieren
 
@@ -15,11 +16,27 @@
 
 ;; -----------------------------------------------------------------------------
 
+(defn row->tr
+  "Konvertiere eine Zeile aus der CSV in eine HTML Tabellenzeile."
+  [{:keys [datum minuten kurs beschreibung]}]
+  [:tr
+   [:td datum]
+   [:td minuten]
+   [:td kurs]
+   [:td beschreibung]])
+
 (defn index-page []
   (base
-   [:h1 "Hallo!"]
-   [:p "Dies ist irgendein Text"]
-   [:img {:src "https://www.hhu.de/typo3conf/ext/wiminno/Resources/Public/img/hhu_logo_mobil.png"}]))
+   [:h1 "#TimeTracker"]
+   [:table
+    [:thead
+     [:tr
+      [:th "Datum"]
+      [:th "Minuten"]
+      [:th "Kurs"]
+      [:th "Beschreibung"]]]
+    [:tbody
+     (map row->tr tt/rows)]]))
 
 ;; -----------------------------------------------------------------------------
 
