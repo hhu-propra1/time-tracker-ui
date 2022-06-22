9073b2c30ce47bdafca7a4fadea402058048dcec
Christian Meter <cmeter@googlemail.com>
2022-06-22 18:17:33 +0200

    Add fancy chartjs graph

--- site.clj
+++ site.clj
@@ -1,5 +1,7 @@
 (ns site
-  (:require [hiccup2.core :as html]
+  (:require [clojure.string :as str]
+            [hiccup.util :refer [raw-string]]
+            [hiccup2.core :as html]
             [timetracker :as tt]))
 
 ;; C-c C-c zum S-Expression evaluieren
@@ -11,6 +13,7 @@
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
     [:link {:href "node_modules/bootstrap/dist/css/bootstrap.min.css"
             :rel "stylesheet"}]
+    [:script {:src "node_modules/chart.js/dist/chart.min.js"}]
     [:title "Titel"]]
    [:body.container.pt-3
     [:div#main
@@ -27,6 +30,49 @@
    [:td kurs]
    [:td beschreibung]])
 
+(def kurs-zu-minuten
+  "Vorarbeit, um die summierten Minuten pro Kurs zu erhalten."
+  (->> (tt/rows)
+       (group-by :kurs)
+       (map (fn [[kurs entries]]
+              {:kurs kurs
+               :minuten (tt/sum-minutes entries)}))))
+
+(def kurse
+  "Alle eindeutigen Kurse."
+  (->> (map :kurs kurs-zu-minuten)
+       (map #(format "\"%s\"" %))
+       (str/join ",")))
+
+(def minuten
+  "Verbrachte Minuten von den Kursen."
+  (->> (map :minuten kurs-zu-minuten)
+       (str/join ",")))
+
+(def chart
+  [:section
+   [:canvas#chart]
+   [:script
+    (raw-string
+     (format
+      "
+      const data = {
+        labels: [%s],
+        datasets: [{
+          label: 'Timetracker',
+          data: [%s]
+        }]
+      }
+      const config = {
+        type: 'bar',
+        data: data,
+        options: {}                  
+      }
+      const myChart = new Chart(
+        document.getElementById('chart'),
+        config
+      );" kurse minuten))]])
+
 (defn index-page []
   (base
    [:h1 "#TimeTracker"]
@@ -38,7 +84,8 @@
       [:th "Kurs"]
       [:th "Beschreibung"]]]
     [:tbody
-     (map row->tr (tt/rows))]]))
+     (map row->tr (tt/rows))]]
+   chart))
 
 ;; -----------------------------------------------------------------------------
 
